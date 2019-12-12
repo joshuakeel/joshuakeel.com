458fb0de8a188d6ef01a2da1c3f16763f4855c45
Joshua Keel <joshuakeel@gmail.com>
2019-12-11 20:30:30 -0500

    Add Eleventy transform to minify HTML with html-minifier.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const { DateTime } = require('luxon');
+const htmlmin = require('html-minifier');
 
 module.exports = function(eleventyConfig) {
 
@@ -8,9 +9,23 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addCollection('mostRecentPosts', collection => {
         return collection.getFilteredByTag('post')
-          .reverse()
-          .slice(0, 3);
-      });
+            .reverse()
+            .slice(0, 3);
+    });
+
+    eleventyConfig.addTransform('htmlmin', function(content, outputPath) {
+        if(outputPath.endsWith('.html')) {
+            let minified = htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true
+            });
+
+            return minified;
+        }
+
+        return content;
+    });
 
     eleventyConfig.addPassthroughCopy("assets");
 };
